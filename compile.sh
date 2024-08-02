549cd0e20deeef16ff73d07638b7461e2c01bce2
James LeBlanc <jpfleblanc@gmail.com>
2024-08-02 11:00:00 -0230

    created testing framework - tests fail

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/path/to/install -DBUILD_DOC=OFF  -DMAKE_PYTAMI=ON -DPYTHON_LIBRARY_DIR=`python -c "import site; print(site.getsitepackages()[0])"` -DPYTHON_EXECUTABLE=`which python` -DCMAKE_PREFIX_PATH=`python -c "import torch; print(torch.utils.cmake_prefix_path)"` ..
+cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/path/to/install -DCMAKE_BUILD_TYPE=Release -DBUILD_DOC=OFF  -DMAKE_PYTAMI=ON -DPYTHON_LIBRARY_DIR=`python -c "import site; print(site.getsitepackages()[0])"` -DPYTHON_EXECUTABLE=`which python` -DCMAKE_PREFIX_PATH=`python -c "import torch; print(torch.utils.cmake_prefix_path)"` ..
 
